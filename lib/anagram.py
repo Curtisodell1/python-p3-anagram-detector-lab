d06252844893516af3d670bbef56f972db3f52ed
Curtis O'Dell <curtisodell1@gmail.com>
2023-08-24 18:13:30 -0600

    working with anagrams - sorted

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,16 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word_letters = sorted([x for x in word])
+
+    def match(self, word_list):
+        matches = []
+
+        for word in word_list:
+            if sorted([x for x in word]) == self.word_letters:
+                matches.append(word)
+
+        return matches
+
+    
